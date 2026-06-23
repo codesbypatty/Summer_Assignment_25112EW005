67fd269888ff3e9f3b10a230ed9b324b576c879f
Patty <text.deepesh@gmail.com>
2026-06-24 00:27:33 +0530

    edited q91

--- Week-4/Day-23/q91.c
+++ Week-4/Day-23/q91.c
@@ -2,23 +2,67 @@
 
 void main()
 {
-    char str[100];
-    int i,j;
+    char str1[100],str2[100];
+    int i,j,len1=0,len2=0,temp;
+    int flag=1;
 
-    printf("enter string : ");
-    gets(str);
+    printf("enter first string : ");
+    gets(str1);
 
-    for(i=0;str[i]!='\0';i++)
+    printf("enter second string : ");
+    gets(str2);
+
+    while(str1[len1]!='\0')
     {
-        for(j=i+1;str[j]!='\0';j++)
+        len1++;
+    }
+
+    while(str2[len2]!='\0')
+    {
+        len2++;
+    }
+
+    if(len1!=len2)
+    {
+        printf("not anagram");
+        return;
+    }
+
+    for(i=0;i<len1-1;i++)
+    {
+        for(j=i+1;j<len1;j++)
         {
-            if(str[i]==str[j])
+            if(str1[i]>str1[j])
             {
-                printf("first repeating character = %c",str[i]);
-                return;
+                temp=str1[i];
+                str1[i]=str1[j];
+                str1[j]=temp;
+            }
+
+            if(str2[i]>str2[j])
+            {
+                temp=str2[i];
+                str2[i]=str2[j];
+                str2[j]=temp;
             }
         }
     }
 
-    printf("no repeating character found");
+    for(i=0;i<len1;i++)
+    {
+        if(str1[i]!=str2[i])
+        {
+            flag=0;
+            break;
+        }
+    }
+
+    if(flag==1)
+    {
+        printf("anagram strings");
+    }
+    else
+    {
+        printf("not anagram");
+    }
 }
